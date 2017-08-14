c5cdb42b0118448a77249cbcbd1dad04129900da
Matt Travi <programmer@travi.org>
2017-08-13 23:19:11 -0500

    WIP(header) used the material-ui AppBar for the header

--- test/unit/molecules/header/header-test.js
+++ test/unit/molecules/header/header-test.js
@@ -4,5 +4,10 @@ import {shallow} from 'enzyme';
 import Header from '../../../../src/molecules/header';
 
 suite('header', () => {
-  shallow(<Header />);
+  test('that the app-bar from material-ui is used', () => {
+    const wrapper = shallow(<Header />).dive();
+    const bar = wrapper.find('AppBar');
+
+    assert.isTrue(bar.exists());
+  });
 });
