4c042b594366a8d651d4f5b76712c52a9654d5df
Matt Travi <programmer@travi.org>
2017-08-23 23:29:55 -0500

    WIP(theme): added a hard-coded theme
    currently, the theme only defines the primary color palette

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,54 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+import React from 'react';
+import {configure, addDecorator} from '@storybook/react';
+import {MuiThemeProvider, createMuiTheme, createPalette} from 'material-ui/styles';
 
-import { configure } from '@storybook/react';
+// palette generated at http://mcg.mbitson.com/ from #c80000
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: {
+      '50': '#f8e0e0',
+      '100': '#efb3b3',
+      '200': '#e48080',
+      '300': '#d94d4d',
+      '400': '#d02626',
+      '500': '#c80000',
+      '600': '#c20000',
+      '700': '#bb0000',
+      '800': '#b40000',
+      '900': '#a70000',
+      'A100': '#ffd1d1',
+      'A200': '#ff9e9e',
+      'A400': '#ff6b6b',
+      'A700': '#ff5252',
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': [
+        '50',
+        '100',
+        '200',
+        'A100',
+        'A200',
+        'A400',
+        'A700'
+      ],
+      'contrastLightColors': [
+        '300',
+        '400',
+        '500',
+        '600',
+        '700',
+        '800',
+        '900'
+      ]
+    }
+  })
+});
+
+addDecorator(story => (
+  <MuiThemeProvider theme={theme}>
+    {story()}
+  </MuiThemeProvider>
+));
 
 function loadStories() {
   const req = require.context('../src', true, /stories.js$/);
