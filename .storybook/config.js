ca12b4b3f4a4d01c4476e8653f6f7197f2cd164b
Matt Travi <programmer@travi.org>
2018-10-29 23:33:51 -0500

    feat(theme): exposed the mui theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,47 +1,7 @@
 import React from 'react';
 import {configure, addDecorator} from '@storybook/react';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-
-// palette generated at http://mcg.mbitson.com/ from #c80000
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      50: '#f8e0e0',
-      100: '#efb3b3',
-      200: '#e48080',
-      300: '#d94d4d',
-      400: '#d02626',
-      500: '#c80000',
-      600: '#c20000',
-      700: '#bb0000',
-      800: '#b40000',
-      900: '#a70000',
-      A100: '#ffd1d1',
-      A200: '#ff9e9e',
-      A400: '#ff6b6b',
-      A700: '#ff5252',
-      contrastDefaultColor: 'light',
-      contrastDarkColors: [
-        '50',
-        '100',
-        '200',
-        'A100',
-        'A200',
-        'A400',
-        'A700'
-      ],
-      contrastLightColors: [
-        '300',
-        '400',
-        '500',
-        '600',
-        '700',
-        '800',
-        '900'
-      ]
-    }
-  }
-});
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import {theme} from '../src';
 
 addDecorator(story => (
   <MuiThemeProvider theme={theme}>
