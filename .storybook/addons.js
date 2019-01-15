6dd5a05fe629ca84bd65979804af0d286498f90b
Matt Travi <programmer@travi.org>
2019-01-14 22:16:26 -0600

    feat(avatar): made the image a circle

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,2 @@
-import 'regenerator-runtime/runtime';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
--- src/atoms/avatar/index.js
+++ src/atoms/avatar/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {number, string} from 'prop-types';
 
 export default function Avatar({dimensions, src, name}) {
-  return <img className="u-photo" alt={name} width={dimensions} height={dimensions} src={src} />;
+  return (
+    <img className="u-photo" alt={name} width={dimensions} height={dimensions} src={src} css={{borderRadius: '50%'}} />
+  );
 }
 
 Avatar.propTypes = {
