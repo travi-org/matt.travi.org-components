6aa59e00d16ec3e9950f1a9b4ec47d05aa21d965
Matt Travi <programmer@travi.org>
2019-01-18 23:24:37 -0600

    wip(navigation): wrapped layout in a stateful container
    to store and provide the open state of the navigation drawer

---
+++ test/unit/organisms/layout/container-test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {assert} from 'chai';
+import LayoutContainer from '../../../../src/organisms/layout/container';
+
+suite('layout container', () => {
+  const Child = () => null;
+
+  test('that the drawer defaults to closed', () => {
+    const wrapper = shallow(<LayoutContainer><Child /></LayoutContainer>);
+    const presentationalLayout = wrapper.find('Layout');
+
+    assert.isFalse(presentationalLayout.prop('navigationOpen'));
+    assert.isTrue(presentationalLayout.find('Child').exists());
+  });
+
+  test('that the open state of the drawer is toggled by the handler', () => {
+    const wrapper = shallow(<LayoutContainer><Child /></LayoutContainer>);
+    const presentationalLayout = wrapper.find('Layout');
+
+    presentationalLayout.simulate('navigationDrawerToggle');
+    assert.isTrue(wrapper.find('Layout').prop('navigationOpen'));
+
+    presentationalLayout.simulate('navigationDrawerToggle');
+    assert.isFalse(wrapper.find('Layout').prop('navigationOpen'));
+  });
+});
--- test/unit/organisms/layout/presentational-test.js
+++ test/unit/organisms/layout/presentational-test.js
@@ -2,21 +2,27 @@ import React from 'react';
 import {CssBaseline} from '@material-ui/core';
 import {shallow} from 'enzyme';
 import {assert} from 'chai';
-import Layout from '../../../../src/organisms/layout';
+import any from '@travi/any';
+import Layout from '../../../../src/organisms/layout/presentational';
 
 suite('layout', () => {
   const Child = () => null;
   Child.displayName = 'Child';
 
   test('that the layout includes the header and children', () => {
+    const navigationToggleHandler = () => undefined;
+    const navigationOpen = any.boolean();
+
     const wrapper = shallow(
-      <Layout>
+      <Layout navigationOpen={navigationOpen} onNavigationDrawerToggle={navigationToggleHandler}>
         <Child />
       </Layout>
     );
+    const header = wrapper.find('Header');
 
     assert.isTrue(wrapper.find(CssBaseline).exists());
-    assert.isTrue(wrapper.find('Header').exists());
     assert.isTrue(wrapper.find('Child').exists());
+
+    assert.equal(header.prop('onNavigationDrawerToggle'), navigationToggleHandler);
   });
 });
