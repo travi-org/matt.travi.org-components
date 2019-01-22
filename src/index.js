ea8f9bf83510da0fcea618a0a72adb08624251ca
Matt Travi <programmer@travi.org>
2019-01-21 22:11:55 -0600

    fix(theme): created a new intance for each request
    BREAKING CHANGE: the theme is no longer exported since it is only used internally
    
    for travi-org/matt.travi.org#349

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 /* eslint import/prefer-default-export: "off" */
 export {default as Layout} from './organisms/layout';
 export {default as Profile} from './molecules/profile';
-export {default as theme} from './theme';
