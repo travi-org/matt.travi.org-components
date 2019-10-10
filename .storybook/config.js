c80c6a8f491cd7c5ade0d39e9b14d5ebb7e7eac1
Matt Travi <programmer@travi.org>
2019-10-09 22:33:59 -0500

    feat(dark): enabled showing the dark theme if desired by the visitor

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {useMediaQuery} from '@material-ui/core';
 import {addDecorator, configure, getStorybook, setAddon} from '@storybook/react';
 import {withInfo} from '@storybook/addon-info';
 import createPercyAddon from '@percy-io/percy-storybook';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import createTheme from '../src/theme';
 
-addDecorator(story => (
-  <MuiThemeProvider theme={createTheme()}>
-    {story()}
-  </MuiThemeProvider>
-));
+const Wrapper = ({story}) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  return (
+    <MuiThemeProvider theme={createTheme(prefersDarkMode ? 'dark' : 'light')}>
+      {story()}
+    </MuiThemeProvider>
+  );
+};
+
+addDecorator(story => <Wrapper story={story} />);
 
 function loadStories() {
   const req = require.context('../src', true, /stories.js$/);
