8566adfb9870620eec97b6e2a0194a65376d816e
Matt Travi <programmer@travi.org>
2019-12-17 23:55:33 -0600

    fix(alignment): fixed positioning of the main content
    including accounting for the app-bar being positioned over the main content (while im not happy with
    the approach, it is one of the recommendations from the docs
    https://material-ui.com/components/app-bar/#fixed-placement and works for now)

--- src/molecules/profile/stories.js
+++ src/molecules/profile/stories.js
@@ -1,6 +1,13 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import Profile from './index';
+import Layout from '../../organisms/layout';
+import Profile from '.';
 
-storiesOf('Molecules/Profile', module).add('default', () => <Profile />);
+storiesOf('Molecules/Profile', module)
+  .add('default', () => <Profile />)
+  .add('wrapped in layout', () => (
+    <Layout>
+      <Profile />
+    </Layout>
+  ));
