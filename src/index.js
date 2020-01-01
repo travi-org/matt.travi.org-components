58aad41949a8986eba6bc567c7a48cba4f701781
Matt Travi <programmer@travi.org>
2019-12-31 19:37:04 -0600

    feat(contact): exposed the contact form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 /* eslint import/prefer-default-export: "off" */
 export {default as Layout} from './organisms/layout';
 export {default as Profile} from './molecules/profile';
+export {default as Contact} from './molecules/contact-form';
