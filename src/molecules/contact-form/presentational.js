c80398870a95567920a6fccc5884e662c70bddb2
Matt Travi <programmer@travi.org>
2020-03-28 13:48:22 -0500

    feat(contact-form): improved the layout of the form

--- src/molecules/contact-form/presentational.js
+++ src/molecules/contact-form/presentational.js
@@ -5,16 +5,16 @@ import {TextInput, PrimaryButton} from '@travi/travi.org-core-components';
 
 export default function ContactForm({action, name}) {
   return (
-    <form method="POST" action={action} name={name} data-netlify>
+    <form method="POST" action={action} name={name} data-netlify css={{textAlign: 'center', maxWidth: 400}}>
       <input type="hidden" name="form-name" value={name} />
 
-      <ol css={{listStyleType: 'none'}}>
-        <li><TextInput label="Name" id="submitter-name" /></li>
-        <li><TextInput label="Email Address" id="email" type="email" /></li>
-        <li><TextInput label="Message" id="message" multiline /></li>
+      <ol css={{listStyleType: 'none', textAlign: 'left'}}>
+        <li><TextInput label="Name" id="submitter-name" variant="full-width" /></li>
+        <li><TextInput label="Email Address" id="email" type="email" variant="full-width" /></li>
+        <li><TextInput label="Message" id="message" multiline variant="full-width" /></li>
       </ol>
 
-      <ol css={{listStyleType: 'none', display: 'inlineBlock'}}>
+      <ol css={{listStyleType: 'none', display: 'inlineBlock', textAlign: 'right'}}>
         <li><PrimaryButton type="submit" icon={<Send />}>Contact Travi</PrimaryButton></li>
       </ol>
     </form>
